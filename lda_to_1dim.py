13dbbc082ae2b80b5d8310d847a069be9cf313f9
Sonu Dixit <19469232+SonuDixit@users.noreply.github.com>
2018-09-09 21:46:16 +0530

    Add files via upload

---
+++ lda_to_1dim.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 17:28:40 2018
+
+@author: vissim
+"""
+import numpy as np
+def LDA_to_1dim(data,label):
+    #calculate distance between mean in new coordinates
+    # calculate within class scatter matrices.
+    #data is assumend to be in num_exam x dim format
+    # label is assumed to contain 1 and 0
+    data = np.asarray(data)
+    label = np.asarray(label)
+    c1data_index = np.where(label == 0)
+    c2data_index=np.where(label == 1)
+    
+    c1data = data[c1data_index]
+    c2data = data[c2data_index]
+    mean_c1 = np.matrix(np.mean(c1data,axis=0))
+    print("shape of mean_c1 is",mean_c1.shape)
+    mean_c2 = np.matrix(np.mean(c2data,axis=0))
+    c1_cov = np.matmul((c1data-mean_c1).T, c1data-mean_c1)
+    c2_cov = np.matmul((c2data-mean_c2).T,c2data-mean_c2)
+    sw_inverse = np.linalg.inv(c1_cov + c2_cov)
+    sb = np.matmul((mean_c1-mean_c2).T,mean_c1-mean_c2)
+#    print(sb)
+    print("shape of sb is",sb.shape)
+    eig_val,eig_vec = np.linalg.eig(np.matmul(sw_inverse,sb))
+    index = np.argsort(eig_val)
+    reqd_vec = eig_vec[:,index[len(index)-1]]
+    return np.matmul(data,reqd_vec),reqd_vec
+    
+#data=[[1,2,2],[1,1,1],[1,2,3]]
+#label=[0,1,1]
+#data = np.asarray(data)
+#label = np.asarray(label)
+#c1data_index = np.where(label == 0)
+#c2data_index=np.where(label == 1)
+#
+#c1data = data[c1data_index]
+#c2data = data[c2data_index]
---
+++ pca_for_large_dimension.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 15:41:05 2018
+
+@author: vissim
+"""
+import numpy as np
+def pca(data,num_components):
+    #data is mean 0 data.
+    ##data is in num_example X num_features
+    #num features  >>> num examples
+    #data doesnt have first column showing feature names
+    #class labels have been removed from data
+    data = np.asarray(data)
+    num_examples = data.shape[0]
+    num_features = data.shape[1]
+    mean_for_pca = np.mean(data,axis=0)
+    data = data - mean_for_pca
+    if num_examples < num_features and num_components<=num_examples:
+        """
+        calculate data X data.T
+        get its eigen_vals and eig vecs
+        """
+        cov_mat = np.matmul(data,data.T)
+        eig_val,eig_vec = np.linalg.eig(cov_mat)
+        indices = np.argsort(eig_val)
+        eig_vec_forpca = np.zeros(shape = (num_features,num_components))
+        for i in range(num_components):
+#            print("eval =",eig_val[indices[-(i+1)]])
+            eig_vec_forpca[:,i] = np.matmul(data.T,eig_vec[:,indices[-(i+1)]])
+#            print("norm is",np.linalg.norm(eig_vec_forpca[:,i]))
+            eig_vec_forpca[:,i] = eig_vec_forpca[:,i]/np.sqrt(np.abs(eig_val[indices[-(i+1)]]))
+#            print("norm is",np.linalg.norm(eig_vec_forpca[:,i]))
+#        for i in range(eig_vec_forpca.shape[1]):
+#            print("norm is",np.linalg.norm(eig_vec_forpca[:,i]))
+        return eig_vec_forpca,mean_for_pca,np.matmul(data,eig_vec_forpca)
+    else:
+        if num_components < num_features:
+            print("implement conventional pca")
+            cov_mat = np.matmul(data.T,data)
+            eig_val,eig_vec = np.linalg.eig(cov_mat)
+            print("......worked")
+            indices = np.argsort(eig_val)
+            eigvec2= np.zeroes(eig_vec.shape[0],num_components)
+            for i in range(num_components):
+                eigvec2[:,i] = eig_vec[:,-i-1]
+            return eigvec2,np.matmul(data,eigvec2)
+        else:
+            return("cant do...its dimensionality increasing...")
+
+
+#evec,data = pca(np.ones(shape=(2,5)),num_components=2)
